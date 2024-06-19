dab2a309e90daea628de2e7f5179890c4707b1a6
unknown <Skmridul090@gmail.com>
2024-06-19 19:00:15 +0100

    added contract api

--- 2. Development/Backend/Pont_Mbale/BackendAPIs/Views/contractus_views.py
+++ 2. Development/Backend/Pont_Mbale/BackendAPIs/Views/contractus_views.py
@@ -3,6 +3,9 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from drf_spectacular.utils import extend_schema
+from django.core.mail import send_mail
+from django.conf import settings
+from django.utils import timezone
 from BackendAPIs.BackendModels.contractus_model import ContractUs
 from BackendAPIs.Serializers.contractus_serializers import ContractUsSerializers
 
@@ -15,3 +18,33 @@ def create_contract_us(request):
             serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+# @permission_classes([IsAuthenticated])
+def contact_us_list(request):
+    contacts = ContactUs.objects.all()
+    serializer = ContactUsSerializer(contacts, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+# @permission_classes([IsAuthenticated])
+def reply_to_message(request, pk):
+    try:
+        contact_message = ContactUs.objects.get(pk=pk)
+    except ContactUs.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if 'admin_reply' in request.data:
+        contact_message.admin_reply = request.data['admin_reply']
+        send_mail(
+            f"Reply to: {contact_message.subject}",
+            contact_message.admin_reply,
+            settings.DEFAULT_FROM_EMAIL,
+            [contact_message.email],
+            fail_silently=False,
+        )
+        contact_message.replied_at = timezone.now()
+        contact_message.save()
+
+    serializer = ContactUsSerializer(contact_message)
+    return Response(serializer.data, status=status.HTTP_200_OK)
